ee10160a77233e51c6aa7b478972c2742a743fd1
googlyeyesultra <googlyeyesultra@gmail.com>
2024-07-16 12:37:29 -0500

    Support for labels on displayed images.

--- plots.py
+++ plots.py
@@ -2,28 +2,31 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as mtick
 
 
-def show_imgs(imgs, title=None, grayscale=False):  # Grayscale is assuming no channels dimension.
-    img_scale = 10
-    plt.figure(figsize=(img_scale, len(imgs) * img_scale))
+def show_imgs(imgs, title=None, labels=None, grayscale=False):
+    img_scale = 2
+    plt.figure(figsize=(len(imgs)*img_scale, 2.2))
     
     if grayscale:
         plt.gray()
+
+    if title:
+        plt.suptitle(title)
         
     plt.gcf().set_facecolor("pink")  # Helps see alpha vs white.
+    
     for i, img in enumerate(imgs):
         plt.subplot(1, len(imgs), i+1)
+        if labels:
+            plt.title(labels[i])
         plt.axis("off")
         img = img.detach().cpu()
-        if not grayscale:
-            img = (img + 1) / 2  # Convert from [-1, 1] to [0, 1]
+        img = (img + 1) / 2  # Convert from [-1, 1] to [0, 1]
+        if not grayscale:  # Grayscale is assuming no channels dimension.
             img = img.permute(1, 2, 0)  # Move channels last.
         plt.imshow(img)
-    
-    if title:
-        plt.title(title)
         
     plt.show()
-    
+
     
 def plot_stats(stats, labels, title, xlabel="Epochs", percent=False):  # Tuple of lists, tuple of strings, string.
     epochs = len(stats[0])
